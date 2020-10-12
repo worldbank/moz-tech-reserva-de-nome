6b05976de18615a3511d58f4414b50ed68b18809
Eduardo Cuducos <cuducos@users.noreply.github.com>
2020-10-12 15:19:43 -0400

    Uses CheckForm in API availability view

--- reserva/core/views/api.py
+++ reserva/core/views/api.py
@@ -1,12 +1,18 @@
-from django.http import JsonResponse
+from django.http import HttpResponseNotAllowed, JsonResponse
 
-from reserva.core.models import NameApplication as NameApp
+from reserva.core.forms import CheckForm
 
 
 def available(request):
-    return JsonResponse(
-        {
-            "available": NameApp.objects.is_available(request.POST["name"]),
-            "name": request.POST["name"],
-        }
-    )
+    if request.method != "POST":
+        raise HttpResponseNotAllowed(("POST",))
+
+    form = CheckForm(request.POST)
+    if form.is_valid():
+        available = True
+        name = form.cleaned_data["name"]
+    else:
+        available = False
+        name = request.POST["name"]
+
+    return JsonResponse({"available": available, "name": name})
