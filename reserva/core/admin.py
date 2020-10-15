7faafff1969c65a69c802a73ecee6a58a0935560
Eduardo Cuducos <cuducos@users.noreply.github.com>
2020-10-15 18:59:11 -0400

    Lists only users with approvals in filter

--- reserva/core/admin.py
+++ reserva/core/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 
-from reserva.core.models import NameApplication
+from reserva.core.models import NameApplication, User
+
+
+class UsersWithApprovals(admin.SimpleListFilter):
+    title = "Aprovado por"
+    parameter_name = "approved_by"
+
+    def lookups(self, request, model_admin):
+        qs = User.objects.filter(
+            pk__in=NameApplication.objects.exclude(approved_by=None)
+            .values("approved_by__pk")
+            .distinct()
+        )
+        return tuple((user.pk, user.name) for user in qs)
+
+    def queryset(self, request, queryset):
+        if not self.value():
+            return queryset
+
+        return queryset.filter(approved_by=self.value())
 
 
 class NameApplicationModelAdmin(admin.ModelAdmin):
@@ -22,7 +41,7 @@ class NameApplicationModelAdmin(admin.ModelAdmin):
         "created_at",
         "updated_at",
     )
-    list_filter = ("approved", "approved_by", "created_at", "updated_at")
+    list_filter = ("approved", UsersWithApprovals, "created_at", "updated_at")
 
     def save_model(self, request, obj, form, change):
         if obj.approved:
